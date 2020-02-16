0b8e05120659f8c7feee07d6391e3a2fece57cca
Gavril Ioan Hasmasan <gsylverman@gmail.com>
2020-02-16 16:29:43 +0200

    cors added

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+//alow cross origin requests
+app.use(cors());
+
 // connect to mlab database
-// make sure to replace my db string & creds with your own
 mongoose.connect(
   "mongodb://user1:NPiQXorDPAuprcgx@cluster0-shard-00-00-hpnb0.mongodb.net:27017,cluster0-shard-00-01-hpnb0.mongodb.net:27017,cluster0-shard-00-02-hpnb0.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
   { useNewUrlParser: true }
