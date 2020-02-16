c8e9de1bc034428e1681642aaf5455241423f4dd
Gavril Ioan Hasmasan <gsylverman@gmail.com>
2020-02-16 15:50:42 +0200

    client added

---
+++ client/r_app/src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import BookList from "./BookList/BookList";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        APp
+        <BookList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/r_app/src/components/App/BookList/BookList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+
+class BookList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Book1</li>
+      </ul>
+    );
+  }
+}
+
+export default BookList;
